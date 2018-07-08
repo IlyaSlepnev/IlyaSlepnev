62432ecaadf90760a1cc6159783d3403b2f7110e
IlyaSlepnev <40866911+IlyaSlepnev@users.noreply.github.com>
2018-07-08 23:14:39 +0600

    FactoryTests

---
+++ Calc/CalcTests/FactoryTest/OneArgumentFactoryTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Calc.oneArgument;
+
+namespace CalcTests.FactoryTest
+{
+
+    public class OneArgumentFactoryTest
+    {
+        [TestCase("radicalSolution", typeof(Radical))]
+        [TestCase("sinSolution", typeof(Sinus))]
+        [TestCase("cosSolution", typeof(Сosine))]
+        [TestCase("tanSolution", typeof(Tangent))]
+
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = OneArgumentFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+
+        }
+    }
+}
---
+++ Calc/CalcTests/FactoryTest/TwoArgumentsFactoryTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Calc.twoArguments;
+using NUnit.Framework;
+
+namespace CalcTests.FactoryTest
+{
+    public class TwoArgumentsFactoryTest
+    {
+        [TestCase("SolutionPlus", typeof(Plus))]
+        [TestCase("SolutionMinus", typeof(Minus))]
+        [TestCase("SolutionMulti", typeof(Multiplication))]
+        [TestCase("SolutionDiv", typeof(Division))]
+        [TestCase("SolutionDegree", typeof(Degree))]
+        [TestCase("DegreeFraction", typeof(DegreeFraction))]
+        [TestCase("SolutionLog", typeof(Log))]
+
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentsFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+
+        }
+    }
+
+}
